343e3329ed61b14ef4ecd7766fb4e2ccf6124d01
oliveiramnando <oliveiramnando@gmail.com>
2025-03-02 12:00:40 -0500

    implemented stop and reset buttons

--- script.js
+++ script.js
@@ -10,16 +10,22 @@ function start() {
         timer = setInterval(update, 10);
         isRunning = true;
     }
-
-    
 }
 
 function stop() {
-
+    if (isRunning) {
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning = false;
+    }
 }
 
 function reset() {
-
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00.00"
 }
 
 function update() {
@@ -31,5 +37,10 @@ function update() {
     let seconds = Math.floor(elapsedTime / 1000 % 60);
     let milliseconds = Math.floor(elapsedTime % 1000 / 10);
 
-    display.textContent = '';
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
 }
